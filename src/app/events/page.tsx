f9f959852276cb564495aaa560183c2649c54650
krishnateja2314 <144202518+krishnateja2314@users.noreply.github.com>
2025-01-16 16:47:50 +0530

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,53 +7,41 @@ const Events = [
   {
     title: "Opening Ceremony",
     date: "17th january, 2025",
-    time: "6pm ",
-    venue: "Auditorium",
     discription:
       "The ceremony will be graced by the presence of our esteemed Chief Guest, Dr.B S Murty.",
     image: "opeaning.JPG",
   },
   {
-    title: "Otaku Cosplay",
-    date: "23rd january, 2025",
-    time: "9pm",
-    venue: "OAT",
-    discription:
-      "Otaku Cosplay is a cosplay competition where participants dress up as their favorite anime characters.",
-    image: "/diestaLogo.svg",
-  },
-  {
     title: "Battle of Bands",
     date: "25th january, 2025",
-    time: "5:30pm",
-    venue: "Old mess lawns",
     discription:
       "Battle of Bands is a music competition where bands from different collages participate.",
-    image: "/diestaLogo.svg",
-  },
-  {
-    title: "DJ Night",
-    date: "25th january, 2025",
-    time: "9pm",
-    venue: "Old mess lawns",
-    discription:
-      "DJ Night is a music event where participants can enjoy music and dance.",
-    image: "/diestaLogo.svg",
+    image: "/battle of bands.JPG",
   },
   {
     title: "Dance Performance",
-    date: "26th january, 2025",
-    venue: "OAT",
-    time: "4pm",
+    date: "24th january, 2025",
     discription:
       "Dance Performance is a dance competition where teams from all the groups participate.",
     image: "/Dance.JPG",
   },
   {
+    title: "Otaku Cosplay",
+    date: "23rd january, 2025",
+    discription:
+      "Otaku Cosplay is a cosplay competition where participants dress up as their favorite anime characters.",
+    image: "/cosplay.JPG",
+  },
+  {
+    title: "DJ Night",
+    date: "26th january, 2025",
+    discription:
+      "DJ Night is a music event where participants can enjoy music and dance.",
+    image: "/dj.JPG",
+  },
+  {
     title: "Closing Ceremony",
     date: "26th january, 2025",
-    time: "6pm",
-    venue: "OAT",
     discription:
       "All the prize winners will be felicitated during the ceremony and the fest will be concluded with a grand celebration.",
     image: "/closing.JPG",
@@ -79,9 +67,7 @@ const page = () => {
               />
               <div className="flex-row ">
                 <p>{event.discription}</p>
-                <p className="mt-5 text-rose-200">On: {event.date}.</p>
-                <p className=" text-rose-200">Starting from: {event.time}</p>
-                <p className=" text-rose-200">At: {event.venue}</p>
+                <p className="mt-5 text-purple-400">ON {event.date}.</p>
               </div>
             </motion.div>
           </Section>
